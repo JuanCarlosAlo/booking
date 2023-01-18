da40e8e240619c61f6f87f242d210b2751a2630c
JC <jc.alonsome@gmail.com>
2023-01-18 10:45:22 +0100

    mierc

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-const calendar = document.getElementById('calendar');
-const isLeap = year => {
+const calendar = document.getElementById("calendar");
+const formElement = document.getElementById("form");
+const dinners = document.getElementById("dinners");
+const isLeap = (year) => {
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
   } else {
@@ -8,6 +10,7 @@ const isLeap = year => {
 };
 
 let date = new Date();
+
 let currentYear = date.getFullYear();
 
 const months = {
@@ -22,24 +25,62 @@ const months = {
   september: 31,
   october: 30,
   november: 31,
-  december: 30
+  december: 30,
 };
 
 const monthsArray = [
-  'january',
-  'february',
-  'february',
-  'april',
-  'may',
-  'june',
-  'july',
-  'agust',
-  'september',
-  'october',
-  'november',
-  'december'
+  "january",
+  "february",
+  "february",
+  "april",
+  "may",
+  "june",
+  "july",
+  "agust",
+  "september",
+  "october",
+  "november",
+  "december",
 ];
+let currentMonth = monthsArray[date.getMonth()];
+let calendarDay = "";
+
+const deactivateDays = (index, day) => {
+  if (date.getDate() > index + 1) {
+    day.classList.add("disabled");
+  }
+  if (date.getDate() === index + 1) {
+    day.classList.add("today");
+  } else {
+    calendarDay = day;
+  }
+};
 
 const createDays = () => {
-  const fragments = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment();
+
+  for (let index = 0; index < months[currentMonth]; index++) {
+    const day = document.createElement("div");
+    day.classList.add("day");
+    fragment.append(day);
+    deactivateDays(index, day);
+  }
+  calendar.append(fragment);
 };
+
+createDays();
+
+const calendarArragedment = () => {
+  calendar.children[0].style.gridColumnStart = 7;
+};
+
+calendarArragedment();
+
+calendar.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("day") &&
+    !e.target.classList.contains("disabled")
+  ) {
+    dinners.removeAttribute("disabled");
+  }
+});
